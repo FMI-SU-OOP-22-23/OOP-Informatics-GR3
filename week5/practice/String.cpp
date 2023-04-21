947daf0a0268720d208a272d20f7d88ca63d8bb4
Bozhidar Kamenov <bkamenov@bkamenovJMD6R.vmware.com>
2023-04-21 12:37:22 +0300

    Fixed string

--- week5/practice/String.cpp
+++ week5/practice/String.cpp
@@ -3,61 +3,66 @@
 
 class String {
  public:
-  String() : buf(new char[1]), size(1) { buf[0] = '\0'; }
-
-  String(const char* str) { setString(str); }
-
-  String(const String& other) { setString(other.buf); }
-
-  String& operator=(const String& other) {
-    if (this != &other) {
-      clean();
-      setString(other.buf);
-    }
-
-    return *this;
-  }
-
-  unsigned int length() { return size; }
-
-  const char* c_str() const { return buf; }
-
-  const String& append(const String& other) {
-    int tmpSize = size + other.size - 1;
-    char* tmpBuff = new char[tmpSize];
-
-    strcpy(tmpBuff, buf);
-    strcat(tmpBuff, other.buf);
-
-    clean();
-
-    buf = tmpBuff;
-    size = tmpSize;
-
-    return *this;
-  }
-
+  String();
+  String(const char* str);
+  String(const String& other) = delete;
+  String& operator=(const String& other);
   ~String() { clean(); }
 
- private:
-  void clean() {
-    if (buf != nullptr) {
-      delete[] buf;
-      buf = nullptr;
-    }
-    size = 0;
-  }
+  unsigned int length() { return size; }
+  const char* c_str() const { return buf; }
+  const String& append(const String& other);
 
-  void setString(const char* str) {
-    size = strlen(str) + 1;
-    buf = new char[size];
-    strcpy(buf, str);
-    buf[size] = '\0';
-  }
+ private:
+  void clean();
+  void setString(const char* str);
 
   char* buf;
   int size;
 };
+
+String::String() : buf(new char[1]), size(1) { buf[0] = '\0'; }
+
+String::String(const char* str) : buf(nullptr), size(0) { setString(str); }
+
+const String& String::append(const String& other) {
+  int tmpSize = size + other.size - 1;
+  char* tmpBuff = new char[tmpSize];
+
+  strcpy(tmpBuff, buf);
+  strcat(tmpBuff, other.buf);
+
+  clean();
+
+  buf = tmpBuff;
+  size = tmpSize;
+
+  return *this;
+}
+
+String& String::operator=(const String& other) {
+  if (this != &other) {
+    clean();
+    setString(other.buf);
+  }
+
+  return *this;
+}
+
+void String::clean() {
+  if (buf != nullptr) {
+    delete[] buf;
+    buf = nullptr;
+  }
+  size = 0;
+}
+
+void String::setString(const char* str) {
+  size = strlen(str) + 1;
+  buf = new char[size];
+  strcpy(buf, str);
+  buf[size] = '\0';
+}
 
 int main() {
   String a("FirstName");
